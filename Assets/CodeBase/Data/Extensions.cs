75279a330194d45b02841f02843ada26e5fa8598
Ivan Helsing <60290995+Ivan-Helsing@users.noreply.github.com>
2023-12-13 21:31:30 -0500

    - Json Deserialize extension created.

---
+++ Assets/CodeBase/Data/Extensions.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Data
+{
+  public static class Extensions
+  {
+    public static T ToDeserialize<T>(this string json) => 
+      JsonUtility.FromJson<T>(json);
+  }
+}
