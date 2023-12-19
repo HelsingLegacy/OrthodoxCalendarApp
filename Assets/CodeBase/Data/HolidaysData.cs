5f0c8f801653d61c5f40ef95c43f97519887c905
Ivan Helsing <60290995+Ivan-Helsing@users.noreply.github.com>
2023-12-18 23:26:08 -0500

    HolidaysData completely rewrite

--- Assets/CodeBase/Data/HolidaysData.cs
+++ Assets/CodeBase/Data/HolidaysData.cs
@@ -1,36 +1,123 @@
 ﻿using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace CodeBase.Data
 {
-  [Serializable]
-  public class HolidaysData
-  {
-    public int postId;
-    public string title;
-    public string holidayName;
-    public string holidayDate;
-    public string weekName;
-    public string holidayColor;
-    public string liturgyRecommendations;
-    public string content;
-    public string mainImage;
-    public bool holidayCategory;
-    public string holidayFast;
-    public string holidaySpecial;
-    public List<string> holidayDress;
-    public string dayIcons;
-
-    public List<ReadingGroup> readingGroup;
-
-    [System.Serializable]
-    public class ReadingGroup
+    public class HolidayInfo
     {
-      public string holiday_reading_group_title;
-      public string holiday_reading_group_code;
-      public string holiday_reading_group_request;
-      public string holiday_reading_group_text;
-      public string holiday_reading_copyright;
+        [JsonProperty("postId")]
+        public int PostId;
+
+        [JsonProperty("title")]
+        public string Title;
+
+        [JsonProperty("holidayName")]
+        public string HolidayName;
+
+        [JsonProperty("holidayDate")]
+        public DateTime HolidayDate;
+
+        [JsonProperty("weekName")]
+        public string WeekName;
+
+        [JsonProperty("holidayColor")]
+        public string HolidayColor;
+
+        [JsonProperty("liturgyRecommendations")]
+        public string LiturgyRecommendations;
+
+        [JsonProperty("content")]
+        public string Content;
+
+        [JsonProperty("mainImage")]
+        public string MainImage;
+
+        [JsonProperty("holidayCategory")]
+        public List<HolidayCategory> HolidayCategory;
+
+        [JsonProperty("holidayFast")]
+        public HolidayFast HolidayFast;
+
+        [JsonProperty("holidayFastName")]
+        public HolidayFastName HolidayFastName;
+
+        [JsonProperty("holidaySpecial")]
+        public List<HolidaySpecial> HolidaySpecial;
+
+        [JsonProperty("holidayDress")]
+        public List<HolidayDress> HolidayDress;
+
+        [JsonProperty("dayIcons")]
+        public Dictionary<string, string> DayIcons;
     }
-  }
+
+    public class HolidayCategory
+    {
+        [JsonProperty("term_id")]
+        public int TermId;
+
+        [JsonProperty("name")]
+        public string Name;
+
+        [JsonProperty("slug")]
+        public string Slug;
+
+        [JsonProperty("term_group")]
+        public int TermGroup;
+
+        [JsonProperty("term_taxonomy_id")]
+        public int TermTaxonomyId;
+
+        [JsonProperty("taxonomy")]
+        public string Taxonomy;
+
+        [JsonProperty("description")]
+        public string Description;
+
+        [JsonProperty("parent")]
+        public int Parent;
+
+        [JsonProperty("count")]
+        public int Count;
+
+        [JsonProperty("filter")]
+        public string Filter;
+    }
+
+    public class HolidayFast
+    {
+        [JsonProperty("slug")]
+        public string Slug;
+
+        [JsonProperty("value")]
+        public string Value;
+    }
+
+    public class HolidayFastName
+    {
+        [JsonProperty("slug")]
+        public string Slug;
+
+        [JsonProperty("value")]
+        public string Value;
+    }
+
+    public class HolidaySpecial
+    {
+        [JsonProperty("slug")]
+        public string Slug;
+
+        [JsonProperty("value")]
+        public string Value;
+    }
+
+    public class HolidayDress
+    {
+        [JsonProperty("slug")]
+        public string Slug;
+
+        [JsonProperty("value")]
+        public string Value;
+    }
 }
