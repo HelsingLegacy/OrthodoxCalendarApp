46f88516ef92ecce010d2905f5332df3e7c25942
Ivan Helsing <60290995+Ivan-Helsing@users.noreply.github.com>
2023-12-23 23:42:13 -0500

    Holiday static data updated.
    -sprite naming refactoring

--- Assets/CodeBase/ScriptableData/HolidaysBuildingData.cs
+++ Assets/CodeBase/ScriptableData/HolidaysBuildingData.cs
@@ -6,15 +6,15 @@ namespace CodeBase.ScriptableData
   public class HolidaysBuildingData : ScriptableObject
   {
     [Header("Red Holiday Color")]
-    public Color RedHoliday= new Color(0.6039216f, 0.2039216f, 0.1058824f, 1);
-    public Color BackgroundRedHoliday= new Color(0.6039216f, 0.2039216f, 0.1058824f, 1);
+    public Color RedHoliday= new(0.6039216f, 0.2039216f, 0.1058824f, 1);
+    public Color BackgroundRedHoliday= new(0.9215687f, 0.8392158f, 0.7843138f, 1);
 
     [Header("Black Holiday Color")]
-    public Color BlackHoliday= new Color(0.4078432f, 0.4470589f, 0.3490196f, 1);
-    public Color BackgroundBlackHoliday= new Color(0.4078432f, 0.4470589f, 0.3490196f, 1);
+    public Color BlackHoliday= new(0.4078432f, 0.4470589f, 0.3490196f, 1);
+    public Color BackgroundBlackHoliday= new(0.9803922f, 1f, 0.9490197f, 1);
 
     [Space][Header("Holiday Category")]
-    public Sprite CategoryGreatHoliday;
+    public Sprite CategoryHoliday;
     public Sprite CategoryVigil;
     public Sprite CategoryTwelfth;
     public Sprite CategoryEaster;
