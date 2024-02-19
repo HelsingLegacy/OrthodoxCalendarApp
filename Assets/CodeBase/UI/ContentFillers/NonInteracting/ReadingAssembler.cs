a02ff97836704235b31b393c843a58d03b4762e2
Ivan Helsing <60290995+Ivan-Helsing@users.noreply.github.com>
2024-02-18 21:48:20 -0500

    FastRefactoring

--- Assets/CodeBase/UI/ContentFillers/NonInteracting/ReadingAssembler.cs
+++ Assets/CodeBase/UI/ContentFillers/NonInteracting/ReadingAssembler.cs
@@ -7,9 +7,5 @@ namespace CodeBase.UI.ContentFillers.NonInteracting
   {
     public void SetReadings(string text) => 
       GetComponentInChildren<ContentWriter>().SetContent(text);
-    
-    public void SetBackgroundColor(Color color) => 
-      GetComponentInChildren<BackgroundColor>().SetBackground(color);
-
   }
 }
